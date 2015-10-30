86c806d688f5916032f82f90a7398df5ad475780
b3j0f <jlabejof@yahoo.fr>
2015-10-30 04:21:46 +0100

    :add: revision version after fixing UTs

--- b3j0f/conf/__init__.py
+++ b3j0f/conf/__init__.py
@@ -24,22 +24,17 @@
 # SOFTWARE.
 # --------------------------------------------------------------------
 
-"""Main configuration package.
-"""
+"""Main configuration package."""
 
 __all__ = [
-    '__version_info__', '__version__',
+    '__version__',
     'Configuration', 'Category', 'Parameter',
     'Configurable',
     'ConfDriver',
     'conf_paths', 'add_category'
 ]
 
-#: project version info
-__version_info__ = 0, 1, 9, "beta", 0
-#: project version
-__version__ = ".".join(str(v) for v in __version_info__[:3])
-
+from .version import __version__
 from .configurable import Configurable, conf_paths, add_category
 from .model import Configuration, Category, Parameter
 from .driver import ConfDriver
